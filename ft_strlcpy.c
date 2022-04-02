7f4dab8c7b38afbe4852356a2c25d16eb1ab781f
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-04-02 01:45:29 -0300

    C04 - ft_strlcpy 100% consertada

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guda-sil@student.42sp.org.br <guda-sil@    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 01:35:28 by guda-sil@st       #+#    #+#             */
+/*   Updated: 2022/04/02 01:43:56 by guda-sil@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 static	size_t	static_strlen(const char *s)
 {
-	size_t length;
+	size_t	length;
 
 	length = 0;
 	while (s[length] != '\0')
@@ -15,14 +27,16 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	length;
 	size_t	i;
 
+
 	length = static_strlen(src);
+	if (size <= 0)
+		return (length);
 	i = 0;
-	while (i < size - 1)
+	while (src[i] != '\0' && (i < size - 1))
 	{
 		dst[i] = src[i];
 		i++;
 	}
-
-	dst[--i] = '\0';
+	dst[i] = '\0';
 	return (length);
 }
